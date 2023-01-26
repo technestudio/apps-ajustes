85454e6c3d0b2c97ddb15eb4e76ea9403989d88e
julieta <thethyhun@hotmail.com>
2023-01-26 00:53:43 +0000

    esqueleto inicial

---
+++ smart_return_button/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Botón inteligente para devoluciones",
+
+    'summary': """
+        Limita la visibilidad del botón de envío para solo OS facturadas. Incluye botón inteligente para devoluciones. 
+    """,
+
+    'description': """
+        Limita la visibilidad del botón de envío para solo algunos usuarios y si la OS se encuentra facturada. 
+        Incluye un nuevo botón para Devoluciones, visible solo para algunos usuarios y si la OS se encuentra facturada. 
+        Crea grupos de usuarios para la visibilidad de los botones.
+    """,
+
+    'author': "Techne Studio IT & Consulting",
+    'website': "https://technestudioit.com/",
+
+    'category': 'Sales',
+    'version': '0.1',
+
+    'license': 'Other proprietary',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/despacho_devolucion.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
